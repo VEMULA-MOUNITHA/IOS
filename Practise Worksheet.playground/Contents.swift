a9bf44a9067c2e7788fb045fc429d0f9eb338a90
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-08-24 10:49:38 -0500

    First worksheet

---
+++ Practise Worksheet.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+print("Hii",10,12.25)
+
+var ProgrammingLanguage = "Swift"
+print(ProgrammingLanguage)
+print("My favorite programming language is \(ProgrammingLanguage)")
+
+var age = 3
+print(age * 3)
+
+print("""
+Hello
+World!
+""")
+
+print("Hello All,\rWelcome to Swift Programming")
+
+let welcomeMessage : String = "Hello"
+print(welcomeMessage , "All")
