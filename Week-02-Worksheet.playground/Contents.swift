58c670defd67c29a118f077a70ee8668c81415a0
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-08-31 10:18:01 -0500

    Second Worksheet

---
+++ Week-02-Worksheet.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var fact = "Swift is a type safe language"
+var dev = "Development of Swift began in 2010"
+var author = "Swift was created by Chris Lattner"
+fact.count
+fact += ", it has a better memory management"
+dev.append(" by Apple")
