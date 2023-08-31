23dd0578ddbd9ed88c6183f169c311736dd2db7f
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-08-31 10:42:47 -0500

    Second Worksheet

--- Week-02-Worksheet.playground/Contents.swift
+++ Week-02-Worksheet.playground/Contents.swift
@@ -7,3 +7,47 @@ var author = "Swift was created by Chris Lattner"
 fact.count
 fact += ", it has a better memory management"
 dev.append(" by Apple")
+author.lowercased()
+author.uppercased()
+author[author.startIndex]
+author[author.index(before: author.endIndex)]
+dev[dev.startIndex]
+dev[dev.index(before: dev.endIndex)]
+author[author.index(after: author.startIndex)]
+author[author.index(author.startIndex, offsetBy: 4)]
+author[author.index(author.endIndex, offsetBy: -7)]
+fact[fact.index(fact.endIndex, offsetBy: -4)]
+
+var shoppingList = "The shopping list contains: "
+var foodItems = "Cheese, Butter, Chocolate Spread"
+var clothes = "Socks, T-shirts"
+
+if clothes.hasPrefix("Socks")
+{
+print("The first item in clothes is socks")
+}
+else
+{
+    print("socks is not the first item in clothes")
+}
+
+print(foodItems.split(separator: ","))
+
+if clothes.contains(",")
+{
+print("Clothes contains more than one item")
+}
+else
+{
+    print("Clothes contain only one item")
+}
+
+foodItems[foodItems.index(foodItems.startIndex, offsetBy: 8)..<foodItems.endIndex]
+
+clothes.remove(at: clothes.firstIndex(of: "T")!)
+clothes.remove(at: clothes.firstIndex(of: "-")!)
+
+
+print("\(shoppingList),\(clothes)")
+clothes.insert(contentsOf: ", Trousers", at:
+clothes.endIndex)
