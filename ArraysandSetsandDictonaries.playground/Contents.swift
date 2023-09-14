177b515d2a3cbdb3111f635e25091cd2bb3cd4d2
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-09-14 10:49:03 -0500

    Completed changes

--- ArraysandSetsandDictonaries.playground/Contents.swift
+++ ArraysandSetsandDictonaries.playground/Contents.swift
@@ -5,3 +5,20 @@ var numbers:[Int] = [2,3,4]
 print(numbers)
 var emptyArray = [Int] ()
 print(emptyArray)
+var programmingLanguages = ["Swift", "Java", "Python"]
+print(programmingLanguages[0])
+programmingLanguages[0] = "Java Script"
+print(programmingLanguages[0])
+var names:[String] = ["Oliver", "Elijah", "James"]
+print("Before appending \(names)")
+names.insert("Benjamin", at: 2)
+print("After inserting \(names)")
+print(names.count)
+names.sort()
+print("After sorting \(names)")
+names.reverse()
+print("After reversing \(names)")
+names.remove(at: 3)
+print("After removing \(names)")
+names.swapAt(1, 2)
+print("After swapping \(names)")
