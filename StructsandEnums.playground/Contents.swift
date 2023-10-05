d7a6fc488664dbcb0a867adb9db114716232375e
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-10-05 10:39:30 -0500

    final Commit

--- StructsandEnums.playground/Contents.swift
+++ StructsandEnums.playground/Contents.swift
@@ -12,3 +12,25 @@ let course=Course()
 print(course)
 print(course.courseName+" - " + course.semester)
 course.printCourseDetails()
+struct Employee {
+var empName:String
+var empID:Int
+var empSalary:Double
+func salaryAfterDeduction()->Double{
+return self.empSalary*0.65;
+}
+}
+
+
+
+
+var employee=Employee(empName:"Gaurav Rathod",empID:
+34567,empSalary: 100000)
+print("Employee Details: ")
+print("ID = \(employee.empID)")
+print("Name = \(employee.empName)")
+print("Salary = \(employee.empSalary)")
+print("The salary after tax deduction is :\(employee.salaryAfterDeduction())")
+var emp1=employee;
+emp1.empName="Nithish Golama "
+print(employee.empName+" "+emp1.empName)
