bcc3a314baa5e42accc76b991aa582216012eeae
Mounitha Vemula <vemulamounitha09@gmail.com>
2023-10-05 10:19:40 -0500

    Struct done

--- StructsandEnums.playground/Contents.swift
+++ StructsandEnums.playground/Contents.swift
@@ -1,3 +1,14 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Course {
+var courseName="iOS"
+var semester="Fall"
+var numberOfStudents=70
+func printCourseDetails(){
+print("Course Name: \(courseName) \nSemester:\(semester) \nNumber of Students: \(numberOfStudents)")
+}
+}
+let course=Course()
+print(course)
+print(course.courseName+" - " + course.semester)
+course.printCourseDetails()
